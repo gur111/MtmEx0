f1096fc42dae81df2cf84a56b6571defed2ad75b
Gur Telem <gur111telem@gmail.com>
2020-03-22 11:41:26 -0700

    Suppress warning

--- part1/tester.sh
+++ part1/tester.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+# The output of this script should be empty if it succeeds
+# To run this script of the csl3 do the following (while you're in the folder with the executable and the test files):
+# `http://pastebin.com/raw/4UCQDwa2 | sh -`
 
 prog=./mtm_tot
 
-for i in 1 2 3 4
-do
-  $prog < test$i.in > tmptest.out
+for i in 1 2 3 4; do
+  $prog <test$i.in >tmptest.out
   diff tmptest.out test$i.out
 done
